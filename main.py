94adebb512673b1af4825383762059b3c6cbef9f
Kolya12392766 <87275865+Kolya12392766@users.noreply.github.com>
2022-08-17 22:26:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,51 @@
 print("booting 'sol-os'", end="\r")
 import time as t
 t.sleep(1)
-print("               ")
+while True
+    print("                ")
+    print("(1)calc, (2)game2d", end="\r")
+    from os import system, get_terminal_size
+    clear = lambda : system("cls | clear")
+    i = input()
+    clear()
+    if i == "1"
+        n1 = int(input("n1:"))
+        n2 = int(input("n2:"))
+        w = input("?")
+        clear()
+        out = None
+        if w == "+":
+            out = n1 + n2
+        elif w == "-":
+            out = n1 - n2
+        elif w == "sqrt":
+            out = (n1)**0.5
+        elif w == "/":
+            out = n1 / n2
+        elif w == "*":
+            out = n1 * n2
+        elif w == "**":
+            out = n1 ** n2
+        print(out, end="\r")
+    elif i == "2":
+        n = 0
+        cols, rown = get_terminal_size()
+        while True:
+            clear()
+            o = ""
+            for i in range(cols * rown):
+                if i == n:
+                    o += "p"
+                else:
+                    o += "."
+            i = input(":")
+            if i == "w":
+                n -= cols
+            elif i == "a":
+                n -= 1
+            elif i == "s":
+                n += cols
+            elif i == "d":
+                n += 1
+            else:
+                break
